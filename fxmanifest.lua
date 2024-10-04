ed58a0eca0262612313d4e644b0d670c7de30ddf
Musiker15 <info@musiker15.de>
2024-10-04 18:49:18 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,7 @@ games { 'gta5' }
 author 'Musiker15 - MSK Scripts'
 name 'msk_givevehicle'
 description 'Give or Delete Vehicles with Command or Item'
-version '2.0.1'
+version '2.0.2'
 
 shared_scripts {
 	'@es_extended/imports.lua',
